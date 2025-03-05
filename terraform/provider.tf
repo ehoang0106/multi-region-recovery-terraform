d9ab329b0b8bab0af01d26ac48ebcccdaf674da6
ehoang0106 <khoahoang1698@gmail.com>
2025-03-05 12:43:21 -0800

    first commit

---
+++ terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-khoahoang"
+    key    = "terraform-state-multi-region-recovery"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
