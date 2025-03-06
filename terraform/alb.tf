9a8e26d80f5a80ae24d807a8f73615cac34f7c37
Khoa Hoang <khoahoang1698@gmail.com>
2025-03-06 15:58:00 -0800

    doesnt work sad

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,20 +1,20 @@
-#application load balancer
+# #application load balancer listener
 
-resource "aws_lb" "primary_lb" {
-  provider           = aws
-  name               = "primary-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_primary_sg.id]
-  #loop through the subnets
-  subnets            = [aws_subnet.primary_subnet_1.id, aws_subnet.primary_subnet_2.id]
-}
+# resource "aws_lb" "primary_lb" {
+#   provider           = aws
+#   name               = "primary-lb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.web_primary_sg.id]
+# 
+#   subnets            = [aws_subnet.primary_subnet_1.id, aws_subnet.primary_subnet_2.id]
+# }
 
-resource "aws_lb" "secondary_lb" {
-  provider           = aws.secondary
-  name               = "secondary-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_secondary_sg.id]
-  subnets            = [aws_subnet.secondary_subnet_1.id, aws_subnet.secondary_subnet_2.id]
-}
+# resource "aws_lb" "secondary_lb" {
+#   provider           = aws.secondary
+#   name               = "secondary-lb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.web_secondary_sg.id]
+#   subnets            = [aws_subnet.secondary_subnet_1.id, aws_subnet.secondary_subnet_2.id]
+# }
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,58 +1,58 @@
-#route53 for checking health of the instances
+# #route53 for checking health of the instances
 
-resource "aws_route53_health_check" "primary_health_check" {
-  fqdn              = aws_lb.primary_lb.dns_name
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = 2
-  request_interval  = 30
-}
+# resource "aws_route53_health_check" "primary_health_check" {
+#   fqdn              = aws_lb.primary_lb.dns_name
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/"
+#   failure_threshold = 2
+#   request_interval  = 30
+# }
 
-resource "aws_route53_health_check" "secondady_health_check" {
-  fqdn              = aws_lb.secondary_lb.dns_name
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = 2
-  request_interval  = 30
-}
+# resource "aws_route53_health_check" "secondady_health_check" {
+#   fqdn              = aws_lb.secondary_lb.dns_name
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/"
+#   failure_threshold = 2
+#   request_interval  = 30
+# }
 
-resource "aws_route53_record" "primary_record" {
-  zone_id = var.zone_id
-  name    = "khoah.com"
-  type    = "A"
+# resource "aws_route53_record" "primary_record" {
+#   zone_id = var.zone_id
+#   name    = "khoah.com"
+#   type    = "A"
 
-  set_identifier = "primary-region"
-  failover_routing_policy {
-    type = "PRIMARY"
-  }
+#   set_identifier = "primary-region"
+#   failover_routing_policy {
+#     type = "PRIMARY"
+#   }
 
-  alias {
-    name                   = aws_lb.primary_lb.dns_name
-    zone_id                = aws_lb.primary_lb.zone_id
-    evaluate_target_health = true
-  }
+#   alias {
+#     name                   = aws_lb.primary_lb.dns_name
+#     zone_id                = aws_lb.primary_lb.zone_id
+#     evaluate_target_health = true
+#   }
 
-  health_check_id = aws_route53_health_check.primary_health_check.id
+#   health_check_id = aws_route53_health_check.primary_health_check.id
 
-}
+# }
 
-resource "aws_route53_record" "secondary_record" {
-  zone_id = var.zone_id
-  name    = "khoah.com"
-  type    = "A"
+# resource "aws_route53_record" "secondary_record" {
+#   zone_id = var.zone_id
+#   name    = "khoah.com"
+#   type    = "A"
 
-  set_identifier = "secondary-region"
-  failover_routing_policy {
-    type = "SECONDARY"
-  }
+#   set_identifier = "secondary-region"
+#   failover_routing_policy {
+#     type = "SECONDARY"
+#   }
 
-  alias {
-    name                   = aws_lb.secondary_lb.dns_name
-    zone_id                = aws_lb.secondary_lb.zone_id
-    evaluate_target_health = true
-  }
+#   alias {
+#     name                   = aws_lb.secondary_lb.dns_name
+#     zone_id                = aws_lb.secondary_lb.zone_id
+#     evaluate_target_health = true
+#   }
 
-  health_check_id = aws_route53_health_check.secondady_health_check.id
-}
+#   health_check_id = aws_route53_health_check.secondady_health_check.id
+# }
