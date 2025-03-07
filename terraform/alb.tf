98b7c79e5f3f28628c2986c018ab7d180c8e0582
Khoa Hoang <khoahoang1698@gmail.com>
2025-03-07 08:12:34 -0800

    done with alb

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -9,3 +9,23 @@ resource "aws_lb_target_group" "my_tg" {
 }
 
 #load balancer
+resource "aws_lb" "my-lb" {
+  name = "my-lg"
+  internal = false
+  load_balancer_type = "application"
+  vpc_id = aws_vpc.my_vpc.id
+  security_groups = [aws_security_group.my_sg.id]
+  subnets = [for subnet in aws_subnet.public : subnet.id]
+}
+
+#listener
+
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my-lb.arn
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.my_tg.arn
+  }
+}
